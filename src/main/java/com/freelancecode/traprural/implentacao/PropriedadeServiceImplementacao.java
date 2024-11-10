e146443a64646ab38c2f4c424ada436985e7bdf6
fernandorsoul <fernandorsoul@gmail.com>
2024-11-10 11:47:11 -0300

    Entrega da Feature/#14

---
+++ src/main/java/com/freelancecode/traprural/implentacao/PropriedadeServiceImplementacao.java
@@ -0,0 +1,38 @@
+package com.freelancecode.traprural.implentacao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.freelancecode.traprural.entities.PropriedadeRural;
+import com.freelancecode.traprural.repositories.PropriedadeRepository;
+import com.freelancecode.traprural.services.PropriedadeService;
+@Service
+public class PropriedadeServiceImplementacao implements PropriedadeService {
+	
+	
+
+	    @Autowired
+	    private PropriedadeRepository propriedadeRepository;
+
+	    @Override
+	    public List<PropriedadeRural> findAll() {
+	        return propriedadeRepository.findAll();
+	    }
+
+	    @Override
+	    public PropriedadeRural findById(Long id) {
+	        return propriedadeRepository.findById(id).orElse(null);
+	    }
+
+	    @Override
+	    public PropriedadeRural save(PropriedadeRural propriedade) {
+	        return propriedadeRepository.save(propriedade);
+	    }
+
+	    @Override
+	    public void deleteById(Long id) {
+	        propriedadeRepository.deleteById(id);
+	    }
+	}
