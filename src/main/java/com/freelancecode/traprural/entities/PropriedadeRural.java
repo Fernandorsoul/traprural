68b78e43eecc2afc929b12116290f7867dcf670e
fernandorsoul <fernandorsoul@gmail.com>
2024-11-10 11:33:26 -0300

    Entrega da correção/#02

--- src/main/java/com/freelancecode/traprural/entities/PropriedadeRural.java
+++ src/main/java/com/freelancecode/traprural/entities/PropriedadeRural.java
@@ -1,13 +1,13 @@
 package com.freelancecode.traprural.entities;
-import lombok.Data;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
 @Data
-public class propriedadeRural {
+public class PropriedadeRural {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
